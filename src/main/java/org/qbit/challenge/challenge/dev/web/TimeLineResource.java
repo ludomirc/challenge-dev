a31f01040f8ec10660abca60eba0c2c4e239d416
benek <beniamin.czaplicki@gmail.com>
2018-02-28 00:28:24 +0100

    added TimeLineResource

--- src/main/java/org/qbit/challenge/challenge/dev/web/TimeLineResource.java
+++ src/main/java/org/qbit/challenge/challenge/dev/web/TimeLineResource.java
@@ -1,5 +1,7 @@
 package org.qbit.challenge.challenge.dev.web;
 
+import org.qbit.challenge.challenge.dev.service.TimeLineService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,10 +13,15 @@ import javax.validation.constraints.Size;
 @RequestMapping("/v001")
 public class TimeLineResource {
 
+    @Autowired
+    TimeLineService timeLineService;
+
     @PostMapping("/time-line/{userId}/{observedUserId}")
     ResponseEntity<?> createTimeLine(
             @Valid @Size(min = 1, max = 50) @PathVariable String userId,
-            @Valid @Size(min = 1, max = 50) @PathVariable String followedUserId) {
+            @Valid @Size(min = 1, max = 50) @PathVariable String observedUserId) {
+
+        timeLineService.saveTimeLine(userId,observedUserId);
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
